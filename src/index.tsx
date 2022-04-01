66a1e6dcd024cf6c538327a69a38860f612de648
Anthalasath <chapchico@live.com>
2022-04-01 22:22:07 +0200

    rename to lowercase

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { BreedableNFTForm } from './breedableNFTForm/BreedableNFTForm';
+import { BreedableNFTForm } from './breedableNFTForm/breedableNFTForm';
 
 class App extends React.Component {
   render() {
