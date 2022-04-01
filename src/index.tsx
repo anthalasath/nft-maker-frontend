eb00aa2a6ba0fb39f36c8bfd3c6772b1a811bcf6
Anthalasath <chapchico@live.com>
2022-04-01 20:59:04 +0200

    initial commit, display basic form

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,105 @@
+import { Button, Divider, Grid, Stack, TextField } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BigNumber } from "ethers";
+import AddBoxIcon from '@mui/icons-material/AddBox';
+
+interface PicturePartCategoryViewState {
+  name: string
+  posX: number
+  posY: number
+  pictureUris: string[]
+}
+
+class PicturePartCategoryView extends React.Component<{}, PicturePartCategoryViewState> {
+
+
+}
+
+interface BreedableNFTFormState {
+  name?: string
+  symbol?: string
+  breedingFeeInWei?: BigNumber
+  fatherGeneChance?: number
+  motherGeneChance?: number
+}
+
+class BreedableNFTForm extends React.Component<{}, BreedableNFTFormState> {
+
+  constructor(props: {}) {
+    super(props);
+    this.state = {}
+  }
+
+  handleNameChange(value: string) {
+    this.setState({
+      name: value
+    });
+  }
+
+  handleSymbolChange(value: string) {
+    this.setState({
+      symbol: value
+    });
+  }
+
+  handleBreedingFeeInWeiChange(value: string) {
+    this.setState({
+      breedingFeeInWei: BigNumber.from(value)
+    });
+  }
+
+  handleFatherGeneChanceChange(value: string) {
+    this.setState({
+      fatherGeneChance: Number.parseInt(value)
+    });
+  }
+
+  handleMotherGeneChanceChange(value: string) {
+    this.setState({
+      motherGeneChance: Number.parseInt(value)
+    });
+  }
+
+  handleAddCategoryClick() {
+    alert("Clicked");
+  }
+
+  render() {
+    return <Stack spacing={1}>
+      <TextField label="name" onChange={e => this.handleNameChange(e.target.value)}>
+      </TextField>
+      <TextField label="symbol" onChange={e => this.handleSymbolChange(e.target.value)}>
+      </TextField>
+      <TextField label="breedingFeeInWei" onChange={e => this.handleBreedingFeeInWeiChange(e.target.value)}>
+      </TextField>
+      <TextField label="fatherGeneChance" onChange={e => this.handleFatherGeneChanceChange(e.target.value)}>
+      </TextField>
+      <TextField label="motherGeneChance" onChange={e => this.handleMotherGeneChanceChange(e.target.value)}>
+      </TextField>
+      <Button variant="contained" onClick={() => this.handleAddCategoryClick()}>
+        <AddBoxIcon></AddBoxIcon>
+      </Button>
+    </Stack>
+  }
+}
+
+class App extends React.Component {
+
+  render() {
+    return <Grid container>
+      <Grid item xs={4}></Grid>
+      <Grid item xs={4}>
+        <BreedableNFTForm></BreedableNFTForm>
+      </Grid>
+      <Grid item xs={4}></Grid>
+    </Grid>
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App></App>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
